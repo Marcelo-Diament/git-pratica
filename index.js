90a3fe9952e3bcff53a0a63a7f1921cdd086ac3e
Marcelo-Diament <marcelodiament@gmail.com>
2021-07-21 21:29:57 -0300

    feature: função pronta Henrique

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /**
- * TODO
- * Inserir a função aqui embaixo!
+ * FUNÇÃO QUE MOSTRA O TIPO DE USUÁRIO
  */
+ function ehAdministrador(usuario) {
+  if(usuario.administrador === true) {
+     console.log('O usuário é administrador')
+  } else if (usuarios.administrador === false) {
+     console.log('O usuário é comum')
+  }
+}
+ehAdministrador('Administrador')
