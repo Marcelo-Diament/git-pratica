0f18ac9aa91f9658752eed4f00281c76976cf55c
SerraZ3 <serra.henrique3@gmail.com>
2021-07-21 22:01:22 -0300

    feat: refatorando o codigo melhor que o marcelo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * FUNÇÃO QUE MOSTRA O TIPO DE USUÁRIO
  */
- function ehAdministrador(usuario) {
-  if(usuario.administrador === true) {
-     console.log('O usuário é administrador')
-  } else if (usuario.administrador === false) {
-     console.log('O usuário é comum')
+function ehAdministrador(usuario) {
+  if (usuario.administrador) {
+    console.log("O usuário é administrador");
+  } else {
+    console.log("O usuário é comum");
   }
 }
-ehAdministrador({administrador: true})
+ehAdministrador({ administrador: true });
